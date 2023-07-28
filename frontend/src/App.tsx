64f019b85c532be27f740906e712b7617065dad8
melany.estevez <mel.estevez99@gmail.com>
2023-07-28 04:19:31 -0400

    feat: coneccion con backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,82 @@
-import * as React from 'react';
-import { TextField } from '@mui/material';
-import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import dayjs from 'dayjs';
+import * as React from "react";
+import { TextField } from "@mui/material";
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import axios from "axios";
 function App() {
   // Set the min and max dates using dayjs
   type datesLimits = {
-    minDate: string
-    maxDate: string
-  }
-  type datesOfForm= {
-    userDate : datesLimits
-  }
-  const [userdates,setUserdates] = React.useState<datesOfForm>({
-    userDate:{
-      "minDate": "20/07/2023",
-      "maxDate": "28/07/2023"
-    }
-  })
-  const minDate = dayjs(userdates.userDate.minDate, "DD/MM/YYYY"); // Replace '1 year' with your desired min date offset
-  const maxDate = dayjs(userdates.userDate.maxDate, "DD/MM/YYYY"); // Replace '1 year' with your desired max date offset
+    minDate: string;
+    maxDate: string;
+  };
+  type datesOfForm = {
+    userDate: datesLimits;
+    name: string
+  };
+  const [userdates, setUserdates] = React.useState<datesOfForm>({
+    userDate: {
+      minDate: "20/07/2023",
+      maxDate: "28/07/2023",
+    },
+    name :'KLLL'
+  });
+  const minDate = dayjs().add(-1,'year'); // Replace '1 year' with your desired min date offset
+  const maxDate = dayjs().add(1,'year'); // Replace '1 year' with your desired max date offset
   const [selectedDate, setSelectedDate] = React.useState(null);
 
   const handleDateChange = (date) => {
-    setSelectedDate(date);
+    //setSelectedDate(date);
   };
+  const onSave =()=>{
+    const test = {
+      userDate: {
+        minDate: "01/06/2022",
+        maxDate: "08/07/2024",
+      },
+      name :'KL1'
+    }
+    axios
+      .post("http://localhost:5001/saveData", test)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  const fetchData = () => {
+    axios.get('http://localhost:5001/getData')
+    .then((response) => {
+    setUserdates(response.data);
+    })
+    .catch((error) => {
+    console.error(error);
+    });
+    };
+  React.useEffect(()=>{
 
+  },[userdates])
   return (
+    <>
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label="Select Date"
         value={selectedDate}
         onChange={handleDateChange}
-        minDate={minDate}
-        maxDate={maxDate}
+        minDate={ userdates.userDate?dayjs(userdates.userDate.minDate): minDate}
+        maxDate={ userdates.userDate?dayjs(userdates.userDate.maxDate): maxDate}
         renderInput={(params) => <TextField {...params} />}
       />
     </LocalizationProvider>
+    <button onClick={async()=>{
+      await onSave()
+      await fetchData()
+    }}>
+      save
+    </button>
+    <pre>{JSON.stringify(userdates, null, 2)}</pre>
+    </>
   );
 }
 
